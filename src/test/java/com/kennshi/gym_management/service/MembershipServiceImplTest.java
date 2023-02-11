3c659c08375ac8ae5d7827a9fe55056ab894f90e
Eduard <edwardbs876@gmail.com>
2023-02-11 17:17:56 +0200

    Removed a test

--- src/test/java/com/kennshi/gym_management/service/MembershipServiceImplTest.java
+++ src/test/java/com/kennshi/gym_management/service/MembershipServiceImplTest.java
@@ -2,22 +2,12 @@ package com.kennshi.gym_management.service;
 
 import com.kennshi.gym_management.api.v1.mapper.ClientMapper;
 import com.kennshi.gym_management.api.v1.mapper.MembershipMapper;
-import com.kennshi.gym_management.api.v1.model.MembershipDto;
-import com.kennshi.gym_management.domain.Client;
-import com.kennshi.gym_management.domain.Membership;
-import com.kennshi.gym_management.domain.MembershipType;
 import com.kennshi.gym_management.repositories.ClientRepository;
 import com.kennshi.gym_management.repositories.MembershipRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
 
 class MembershipServiceImplTest {
 
@@ -42,24 +32,6 @@ class MembershipServiceImplTest {
 
     @Test
     void createNewMembership() {
-        //given
-        Client client = new Client();
-        client.setId(1L);
-        client.setName("Busuioc Eduard");
-        client.setEmail("anymail@gmail.com");
-        client.setBirthday(LocalDate.of(2004, 1, 30));
-        client.setPhone("123456789");
-        clientRepository.save(client);
-
-        Membership membership = new Membership(client, new MembershipType());
-        membershipRepository.save(membership);
-
-        when(clientRepository.findById(anyLong())).thenReturn(Optional.of(client));
-
-        MembershipDto savedMembership = membershipService.createNewMembership(1L, membershipMapper.toMembershipDTO(membership));
-
-        //return client with assigned membership
-
 
     }
 }
